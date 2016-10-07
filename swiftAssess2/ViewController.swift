795cb651a9ef38b02b2e36612b021e64b3bba35b
ZD <dalezachary4@gmail.com>
2016-10-07 07:55:34 -0400

    Final, all working

--- swiftAssess2/ViewController.swift
+++ swiftAssess2/ViewController.swift
@@ -8,12 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var table1: UITableView!
+    
+    var rowNum=["Row A", "Row B","Row C","Row D","Row E", "Row F", "Row G", "Row H", "Row I", "Row J", "Row K"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        table1.dataSource=self
+        table1.delegate=self
+        
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return rowNum.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(indexPath.row)
+        
+        let cell=UITableViewCell()
+        
+        cell.textLabel?.text=rowNum[indexPath.row]
+        
+        return cell
+    }
+
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "😡")
+    }
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
